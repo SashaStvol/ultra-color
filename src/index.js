87ae663d6be9fcbe985b175b0941e490f9c626c6
SashaStvol <138790716+SashaStvol@users.noreply.github.com>
2025-11-07 13:55:04 +0700

    Create src/index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * Ultra Color - Main entry point
+ * @module ultra-color
+ */
+
+export { Color } from './Color.js';
+
+// Convenience named exports
+export { 
+    hexToNumber, 
+    numberToHex, 
+    rgbToNumber, 
+    numberToRgb,
+    rgbaToNumber,
+    numberToRgba,
+    isHex,
+    hsvToRgb,
+    rgbToHsv,
+    generatePaletteHSV,
+    generatePaletteGolden
+} from './Color.js';
